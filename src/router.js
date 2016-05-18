3c92038bb5313a432e7da9d915afc2bb3985ad64
elliott-ribner <elr886@gmail.com>
2016-05-18 10:15:16 -0400

    oath token returned, still need to persist this in local storage

--- src/router.js
+++ src/router.js
@@ -3,6 +3,8 @@ import Router from 'ampersand-router';
 import Public from './public';
 import Repos from './repos';
 import Layout from './layout.js';
+import qs from 'qs'
+import xhr from 'xhr'
 
 export default Router.extend({
   renderPage (page, opts = {layout: true}) {
@@ -18,6 +20,8 @@ export default Router.extend({
   routes: {
     '': 'public',
     'repos': 'repos',
+    'login': 'login',
+    'auth/callback?:query': 'authCallback'
   },
   public() {
     this.renderPage(<Public />, {layout: false});
@@ -26,5 +30,23 @@ export default Router.extend({
   repos() {
     this.renderPage(<Repos />)
     console.log('repos');
+  },
+  login() {
+    window.location = 'https://github.com/login/oauth/authorize?' + qs.stringify({
+      client_id: '2876002badfff69aa3f7',
+      redirect_uri: window.location.origin + '/auth/callback',
+      scope: 'user, repo'
+    })
+  },
+  authCallback(query) {
+    query = qs.parse(query);
+    console.log(query)
+
+    xhr({
+      url: 'https://masters-react.herokuapp.com/authenticate/' + query.code,
+      json: true
+    }, (err,req,body) => {
+      console.log(body);
+    })
   }
 })
