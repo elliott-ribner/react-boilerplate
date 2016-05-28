be84f88c894a470870b81bc68108b6dff605a468
elliott-ribner <elr886@gmail.com>
2016-05-28 10:58:51 -0400

    updating labels working, start of creating new labels

--- src/label-item.js
+++ src/label-item.js
@@ -1,33 +1,79 @@
 import React from 'react'
 import ampersandMixin from 'ampersand-react-mixin'
 
+
 export default React.createClass({
   mixins: [ampersandMixin],
   displayName: 'Labels',
-  toggleEdit() {
-    this.props.label.editing = !this.props.label.editing;
+  getInitialState() {
+    const {name, color} = this.props.label
+    return {
+      name: name,
+      color: color 
+    }
+  },
+  showEdit(event) {
+    event.preventDefault();
+    this.props.label.editing = true;
+  },
+  dontShowEdit(event) {
+    event.preventDefault();
+    this.props.label.editing = false;
+    this.setState(this.getInitialState())
+  },
+  clearLabel() {
+    // event.preventDefault();
+    // this.props.label.destroy({
+    //   success: function() {
+    //     alert('label removed');
+    //   },
+    //   error: function() {
+    //     alert('There was an error destroying the label');
+    //   }
+    // })
+    alert('label would be removed but this is commented out so i dont edit these.')
+  },
+  onNameChange(event) {
+    event.preventDefault();
+    this.setState({
+      name: event.target.value
+    })
+  },
+  onColorChange(event) {
+    event.preventDefault();
+    this.setState({
+      color: event.target.value
+    })
+  },
+  onSubmit(event) {
+    event.preventDefault();
+    const {label} = this.props
+    label.update(this.state);
+    label.editing = false;
   },
   render() {
     const {label} = this.props
     let content
-    const cssColor = '#'+ label.color
+    const {color} = this.state 
+    const cssColor = '#' + color
+
     // editing
     if (label.editing) {
       content = ( 
-        <form className='label'>
+        <form onSubmit={this.onSubmit} className='label'>
         <span className='label-color avatar avatar-small avatar-rounded'>&nbsp;</span>
-        <input name='name'/>
-        <input name='color'/>
+        <input name='name' onChange={this.onNameChange} value={this.state.name}/>
+        <input name='color' onChange={this.onColorChange}value={this.state.color}/>
         <button type='submit' className='button button-small'>Save</button>
-        <button type='button' className='button button-small button-unstyled'>cancel</button>
+        <button type='button' className='button button-small button-unstyled' onClick={this.dontShowEdit}>cancel</button>
       </form> )
     } else {
       content = (
         <div className='label'>
           <span className='label-color' style={{backgroundColor: cssColor}}>&nbsp;</span>
           <span>{label.name}</span>
-          <span className='octicon octicon-pencil' onClick={this.toggleEdit}></span>
-          <span className='octicon octicon-x'></span>
+          <span className='octicon octicon-pencil' onClick={this.showEdit}></span>
+          <span className='octicon octicon-x' onClick={this.clearLabel}></span>
         </div>
       )
     }
